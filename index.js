66adaa974775085d8d610d5f5eb4b30725fc6bdb
FedericoBrunfman <federico.brunfman@patientpop.com>
2021-12-13 12:54:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+
+
+// set the home page route
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log('Node app is working!');
+});
